30657e1e5869c2ca569fa8e0ab86ddc3657d85fc
Nadim Farh <201720348@ua.edu.lb>
2021-07-22 19:54:50 +0300

    Last Commit

--- index.js
+++ index.js
@@ -2,19 +2,35 @@ var express = require("express");  // Importing the express js module
 
 var app = express();  // Initializing app using express
 
+app.use(express.json())
+
 app.get('/', (req, res)=> {
     res.send("Hello World");
 });
 
+const users = [{ name: 'Nadim' , password: '1234' }, 
+               { name: 'Charles' , password: '0000' }
+];
+
 app.get('/getUser', (req,res) => {
+    res.json(users)
+});
 
-    var userObj = {
-        userId: 1,
-        userName: "Nadim",
-        active: true
-    }
+app.post('/users' , (req,res) => {
 
-    res.send(userObj);
+    const user = { name: req.body.name, password: req.body.password }
+    users.push(user)
+    res.status(201).send('User created')
+});
+
+app.post('/users/login' , async (req,res) => {
+
+    const user = users.find(user => user.name = req.body.name)
+
+    if (user == null) {
+        return res.status(400).send('User not found')
+    } else
+    return res.status(200).send('User found : ' +res.body.name)
 });
 
 app.listen(4000);
